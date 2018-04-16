76a84d6cc71a56022b0f6709f4722debc638d8f8
aa <joeball@Joes-MacBook-Pro.local>
2018-04-16 20:48:24 +0100

    A

--- quoteRymill.js
+++ quoteRymill.js
@@ -71,7 +71,7 @@ rymillQuotes[68] = "How do you spell it again?";
 rymillQuotes[69] = "Look at me when I’m talking to you – it’s weird!";
 rymillQuotes[70] = "Yenno, I told Mrs Rannard to mine bitcoin on the school computers. That would’ve paid for our pensions!";
 rymillQuotes[71] = "Sto imparando l’italiano – guess what that means!";
-rymillQuotes[72] = "A library overcharged me... So what did I do? I DDoSed them";
+rymillQuotes[72] = "A library overcharged me... So what did I do? I DDoSed them – that’ll teach them not to overcharge people again. It was down for a good few hours";
 
 var rymillSpeech = new Array();
 rymillSpeech[0] = "says";
